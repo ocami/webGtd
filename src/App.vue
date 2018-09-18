8aa172714211798f5a51679c0799fd3e2dad33e3
alex <contact@ocami.fr>
2018-09-18 21:16:42 +0200

    step3 ok

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,80 @@
 <template>
-  <section>
-      <form-location v-if="test"></form-location>
+    <section>
+        <p>locationData</p>
+        <pre>{{locationData}}</pre>
 
-      <!--<b-row>-->
-          <!--<b-col cols="3">-->
-              <!--<menu-layout></menu-layout>-->
-          <!--</b-col>-->
-          <!--<b-col cols="9">-->
-              <!--<edit-layout></edit-layout>-->
-          <!--</b-col>-->
-      <!--</b-row>-->
 
-  </section>
+        <form-location
+                :locationData="locationData"
+                @location-change="locationChange"
+                v-if="test"></form-location>
+
+        <!--<b-row>-->
+        <!--<b-col cols="3">-->
+        <!--<menu-layout></menu-layout>-->
+        <!--</b-col>-->
+        <!--<b-col cols="9">-->
+        <!--<edit-layout></edit-layout>-->
+        <!--</b-col>-->
+        <!--</b-row>-->
+
+    </section>
 </template>
 
 <script>
 
-  import Test from './components/test/model.vue'
-  import app_store from './store/app_store'
-  import MenuLayout from './components/menu/MenuLayout.vue'
-  import EditLayout from './components/edit/EditLayout.vue'
-  import FormLocation from './components/tools/FormLocation.vue'
+    import Test from './components/test/model.vue'
+    import app_store from './store/app_store'
+    import MenuLayout from './components/menu/MenuLayout.vue'
+    import EditLayout from './components/edit/EditLayout.vue'
+    import FormLocation from './components/tools/FormLocation.vue'
 
 
-  export default {
-      name: 'app',
-      components : {
-          Test,
-          MenuLayout,
-          EditLayout,
-          FormLocation
-      },
-      data ()  {
-          return {
-            data: app_store.data,
-            test: true
-          }
-      },
-      computed:{
-      }
+    export default {
+        name: 'app',
+        components: {
+            Test,
+            MenuLayout,
+            EditLayout,
+            FormLocation,
+        },
+        data() {
+            return {
+                data: app_store.data,
+                test: true,
+                locationData: {
+                    idApi: 'ADRNIVX_0000000357696764',
+                    cityQuery: 'Amanlis - 35',
+                    city: 'Amanlis',
+                    cityCode: '35002',
+                    addressQuery: "38 Néron",
+                    address: "38 Néron",
+                    postcode: '35150',
+                    x: -1.477557,
+                    y: 47.97989
+                },
+                // locationData: {
+                //     idApi: null,
+                //     cityQuery: '',
+                //     city: null,
+                //     citycode: null,
+                //     addressQuery: '',
+                //     address: null,
+                //     postcode: null,
+                //     x: 0,
+                //     y: 0
+                // }
+            }
+        },
+        computed: {},
+        methods: {
+            locationChange: function (locationData) {
+                console.log('locationChange')
+                this.locationData = locationData
+            }
+        }
 
-  }
+    }
 </script>
 
 <style>
--- src/components/tools/FormLocation.vue
+++ src/components/tools/FormLocation.vue
@@ -1,27 +1,114 @@
 <template>
     <section>
-        <p>FormLocation</p>
-        <p>{{data.name}}</p>
+        <vue-bootstrap-typeahead
+                class="mb-4"
+                v-model="cityQuery"
+                :data="cities"
+                :serializer="item =>  item.nom +' - '+ item.departement.code"
+                @hit="citySelected($event)"
+                placeholder="Ville, Commune"
+                @input="setCity"
+        />
+
+        <vue-bootstrap-typeahead
+                v-if="this.selectedCity"
+                class="mb-4"
+                v-model="addressQuery"
+                :data="addresses"
+                :serializer="item =>  item.properties.name"
+                @hit="addressSelected($event)"
+                placeholder="Adresse"
+        />
     </section>
 </template>
 
 <script>
 
-    import fls from '../../store/FormLocationStore'
+    import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
 
 
     export default {
         name: 'form-location',
-        components : {
+        components: {
+            VueBootstrapTypeahead
         },
-        data ()  {
+        data() {
             return {
-                data: fls.data,
+                result: this.locationData,
+                cities: [],
+                cityQuery: this.locationData.cityQuery,
+                selectedCity: this.locationData.cityCode,
+                addresses: [],
+                addressQuery: this.locationData.addressQuery,
+                selectedAddress: null,
+
             }
         },
-        computed:{
-        }
+        computed: {},
+        props: {
+            locationData: Object
+        },
+        watch: {
+            cityQuery(newCityQuery) {
+                axios.get(`https://geo.api.gouv.fr/communes?nom=${newCityQuery}&boost=population&fields=departement`)
+                    .then((res) => {
+                        this.cities = res.data
+                    })
+            },
+            addressQuery(newAddressQuery) {
+                if (this.selectedCity && newAddressQuery !== '') {
+                    axios.get(`https://api-adresse.data.gouv.fr/search/?citycode=${this.selectedCity.code}&q=${newAddressQuery}`)
+                        .then((res) => {
+                            this.addresses = res.data.features
+                        })
+                }
+            },
+        },
+        methods: {
+            setCity: function () {
+                console.log('setCity')
+                this.selectedCity = null
+                this.selectedAddress = null
+                this.addressQuery = '',
+                    this.result = {
+                        idApi: null,
+                        cityQuery: null,
+                        city: null,
+                        citycode: null,
+                        addressQuery: null,
+                        address: null,
+                        postcode: null,
+                        x: 0,
+                        y: 0
+                    }
+            },
+            citySelected: function (event) {
+                console.log('citySelected')
+                this.selectedCity = {query: this.cityQuery, code: event.code, name: event.nom}
+                axios.get(`https://api-adresse.data.gouv.fr/search/?citycode=${this.selectedCity.code}&q=${this.selectedCity.name}&type=municipality&limit=1`)
+                    .then((res) => {
+                        this.result.cityQuery = this.cityQuery
+                        this.result.idApi = res.data.features[0].properties.id
+                        this.result.city = res.data.features[0].properties.name
+                        this.result.citycode = res.data.features[0].properties.citycode
+                        this.result.x = res.data.features[0].geometry.coordinates[0]
+                        this.result.y = res.data.features[0].geometry.coordinates[1]
+                        this.$emit('location-change', this.result)
+                    })
+            },
+            addressSelected: function (event) {
+                console.log('addressSelected')
 
+                this.result.addressQuery = this.addressQuery
+                this.result.idApi = event.properties.id
+                this.result.address = event.properties.name
+                this.result.postcode = event.properties.postcode
+                this.result.x = event.geometry.coordinates[0]
+                this.result.y = event.geometry.coordinates[1]
+                this.$emit('location-change', this.result)
+
+            }
+        }
     }
 </script>
 
